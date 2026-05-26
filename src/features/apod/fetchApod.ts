63ad830975b6c71eb879ab3c8f23d968e13fcf08
Oleg <139953855+olezhaku@users.noreply.github.com>
2026-05-26 22:14:09 +0900

    change api key

--- src/features/apod/fetchApod.ts
+++ src/features/apod/fetchApod.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { IApod } from "../../types/types";
 import { RootState } from "../../store";
 
-const apiKey = "ppaoZpc5ci0ycvYXqDLiR1l9u9SqgV1hqALMT802&thumbs";
+const apiKey = "eWusmTnHQUUtbc1Ua19qfKGpsvLhU3ojpDla7GFx&thumbs";
 const link = "planetary/apod?";
 let filterValues = "";
 
